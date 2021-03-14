1b3edf2c6e70281ccc1e734c46cfe519240c59a6
Dylan <klj40702@gmail.com>
2021-03-14 12:39:50 +0800

    feat: Remove the server-side injected CSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 
+import { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
@@ -9,6 +10,15 @@ import { reducer } from '@/store';
 const store = createStore(reducer);
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <Provider store={store}>
       <Layout>
