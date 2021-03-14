5e1d9985e15416ac0bca6a9cde0da78dfa33d2e6
Dylan <klj40702@gmail.com>
2021-03-14 13:07:26 +0800

    feat: RWD

--- components/App/styles.ts
+++ components/App/styles.ts
@@ -4,5 +4,9 @@ export const S = {
   Container: styled.div`
     padding: 10px;
     display: flex;
+
+    @media (max-width: 480px) {
+      flex-wrap: wrap;
+    }
   `,
 };
--- components/LotteryTime/styles.ts
+++ components/LotteryTime/styles.ts
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 export const S = {
   Container: styled.div`
     flex: 0 0 300px;
+
+    @media (max-width: 480px) {
+      flex: 1;
+      margin-bottom: 20px;
+    }
   `,
   Title: styled.h3`
     margin-top: 0;
@@ -14,6 +19,7 @@ export const S = {
   `,
   TimeInput: styled.input`
     width: 80px;
+    border: solid 1px #757575;
   `,
   TimeText: styled.span`
     margin: 0 15px 0 8px;
